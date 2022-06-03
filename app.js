e6f249d7f9be4c462d71a126128fe3fe6871add0
libra1919 <aymentli@gmail.com>
2022-06-03 06:31:40 +0100

    final version

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(helmet());
 
 app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
